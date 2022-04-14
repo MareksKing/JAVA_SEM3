a441c6254c20ed78f7ca06afcc41c34aa6fe37e3
MareksKing <robaldsmareks@gmail.com>
2022-04-14 14:27:23 +0300

    Add: New controller MSG

--- src/main/java/com/example/demo/controller/SimpleController.java
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +11,12 @@ public class SimpleController {
 	public String simpleFunc() {
 		System.out.println("KONOTOOROROAKORK");
 		return "home"; //ielādēs home.html lapu
+	}
+	
+	@GetMapping("/msg") //Localhost:8080/simple
+	public String msgFunc(Model model) { ///vajag lai sutitu datus no backend uz frontend
+		model.addAttribute("packet", "Ziņa no backend puses.....");
+		System.out.println("msg kontrollieries ir izsaukts");
+		return "msg-page"; //ielādējam msg-page.html lapu
 	}
 }
